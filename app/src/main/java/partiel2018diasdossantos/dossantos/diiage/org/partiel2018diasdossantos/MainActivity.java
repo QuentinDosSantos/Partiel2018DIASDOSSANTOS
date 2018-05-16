49426edeccf9be5bb855643075464ac1a6eb5acb
QuentinDOSSANTOS <quentin.dossantos@yandex.com>
2018-05-16 13:35:37 +0200

    Initial commit

---
+++ app/src/main/java/partiel2018diasdossantos/dossantos/diiage/org/partiel2018diasdossantos/MainActivity.java
@@ -0,0 +1,13 @@
+package partiel2018diasdossantos.dossantos.diiage.org.partiel2018diasdossantos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
